01ba6a0c7cbac9ef74584314178965e7b7c58e8e
Huy <106498928+Vanhuyne@users.noreply.github.com>
2024-09-03 20:53:03 +0700

    update gateway

--- user-auth-service/src/main/java/com/movierecommendation/user_service/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.movierecommendation.user_service.repository;
-
-import com.movierecommendation.user_service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
